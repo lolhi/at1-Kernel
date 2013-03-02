5cd9d16bc2243571277236e0f906eb2a32de7169
lolhi <lolhi@naver.com>
2013-03-02 21:41:45 +0900

    Revert "rbtree: add prio tree and interval tree tests"
    This reverts commit f8ca5d4ec90d5a0330b9f30c22865a156017dbe1.

--- lib/prio_tree.c
+++ lib/prio_tree.c
@@ -14,7 +14,6 @@
 #include <linux/init.h>
 #include <linux/mm.h>
 #include <linux/prio_tree.h>
-#include <linux/export.h>
 
 /*
  * A clever mix of heap and radix trees forms a radix priority search tree (PST)
@@ -256,7 +255,6 @@ struct prio_tree_node *prio_tree_insert(struct prio_tree_root *root,
 	BUG();
 	return NULL;
 }
-EXPORT_SYMBOL(prio_tree_insert);
 
 /*
  * Remove a prio_tree_node @node from a radix priority search tree @root. The
@@ -306,7 +304,6 @@ void prio_tree_remove(struct prio_tree_root *root, struct prio_tree_node *node)
 	while (cur != node)
 		cur = prio_tree_replace(root, cur->parent, cur);
 }
-EXPORT_SYMBOL(prio_tree_remove);
 
 /*
  * Following functions help to enumerate all prio_tree_nodes in the tree that
@@ -485,4 +482,3 @@ repeat:
 
 	goto repeat;
 }
-EXPORT_SYMBOL(prio_tree_next);
