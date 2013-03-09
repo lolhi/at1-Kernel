d91cfc10f18fbc1719b899311f7063bae1a63be4
lolhi <lolhi@naver.com>
2013-03-09 23:37:06 +0900

    Revert "msm_fb: display: Wait for external vsync before turning off DTV"
    This reverts commit bd2b9b95dc89c1272762bc913cea9aa256794e13.

--- drivers/video/msm/mdp4.h
+++ drivers/video/msm/mdp4.h
@@ -430,7 +430,6 @@ int mdp4_overlay_dtv_set(struct msm_fb_data_type *mfd,
 			struct mdp4_overlay_pipe *pipe);
 int mdp4_overlay_dtv_unset(struct msm_fb_data_type *mfd,
 			struct mdp4_overlay_pipe *pipe);
-void mdp4_overlay_dtv_wait4vsync(void);
 #else
 static inline void  mdp4_overlay_dtv_ov_done_push(struct msm_fb_data_type *mfd,
 			struct mdp4_overlay_pipe *pipe)
@@ -451,11 +450,6 @@ static inline int mdp4_overlay_dtv_unset(struct msm_fb_data_type *mfd,
 			struct mdp4_overlay_pipe *pipe)
 {
 	return 0;
-}
-
-static inline void mdp4_overlay_dtv_wait4vsync(void)
-{
-    /* empty */
 }
 #endif
 
