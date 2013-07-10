9be5043aa99803b4bc88f777b9cf8cc32279a5a0
lolhi <lolhi@naver.com>
2013-07-10 19:23:26 +0900

    fs: add experimental f2fs from git://github.com/nowcomputing/f2fs-backports Park Ju Hyung committed.. https://bitbucket.org/arter97/arter97-cm-i9300

--- arch/arm/mach-msm/devices-msm8x60.c
+++ arch/arm/mach-msm/devices-msm8x60.c
@@ -855,7 +855,6 @@ static struct kgsl_device_platform_data kgsl_3d0_pdata = {
 		},
 	},
 	.init_level = 0,
-	.max_level = 2,
 	.num_levels = 5,
 	.set_grp_async = NULL,
 	.idle_timeout = HZ/5,
@@ -895,26 +894,29 @@ static struct kgsl_device_platform_data kgsl_2d0_pdata = {
 	.pwrlevel = {
 		{
 			.gpu_freq = 266667000,
-			.bus_freq = 3,
+			.bus_freq = 4,
 		},
 		{
 			.gpu_freq = 228571000,
+			.bus_freq = 3,
+		},
+		{
+			.gpu_freq = 200000000,
 			.bus_freq = 2,
 		},
 		{
-			.gpu_freq = 200000000,
+			.gpu_freq = 160000000,
 			.bus_freq = 1,
 		},
 		{
-			.gpu_freq = 200000000,
+			.gpu_freq = 145455000,
 			.bus_freq = 0,
 		},
 	},
-	.init_level = 1,
-	.max_level = 1,
-	.num_levels = 4,
+	.init_level = 2,
+	.num_levels = 5,
 	.set_grp_async = NULL,
-	.idle_timeout = HZ/10,
+	.idle_timeout = HZ/5,
 	.nap_allowed = false,
 	.clk_map = KGSL_CLK_CORE | KGSL_CLK_IFACE,
 #ifdef CONFIG_MSM_BUS_SCALING
@@ -951,26 +953,29 @@ static struct kgsl_device_platform_data kgsl_2d1_pdata = {
 	.pwrlevel = {
 		{
 			.gpu_freq = 266667000,
-			.bus_freq = 3,
+			.bus_freq = 4,
 		},
 		{
 			.gpu_freq = 228571000,
+			.bus_freq = 3,
+		},
+		{
+			.gpu_freq = 200000000,
 			.bus_freq = 2,
 		},
 		{
-			.gpu_freq = 200000000,
+			.gpu_freq = 160000000,
 			.bus_freq = 1,
 		},
 		{
-			.gpu_freq = 200000000,
+			.gpu_freq = 145455000,
 			.bus_freq = 0,
 		},
 	},
-	.init_level = 1,
-	.max_level = 1,
-	.num_levels = 4,
+	.init_level = 2,
+	.num_levels = 5,
 	.set_grp_async = NULL,
-	.idle_timeout = HZ/10,
+	.idle_timeout = HZ/5,
 	.nap_allowed = false,
 	.clk_map = KGSL_CLK_CORE | KGSL_CLK_IFACE,
 #ifdef CONFIG_MSM_BUS_SCALING
